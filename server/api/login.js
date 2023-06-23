1d4b05231fa806fc44be7e29b0af3893dc2715c9
JahongLiu <jahongliu@gmail.com>
2023-06-23 09:10:28 -0400

    translate comments for draft,login,user apis

--- server/api/login.js
+++ server/api/login.js
@@ -1,58 +1,61 @@
-const express = require('express')
-const router = express.Router()
-const secret = require('../config').jwt
-const jwt = require('jsonwebtoken')
-const db = require('../db/db.js')
-const sha1 = require('sha1')
-const rand = require('csprng')
-const formidable = require('formidable')
-const path = require('path')
-const fs = require('fs')
+const express = require("express");
+const router = express.Router();
+const secret = require("../config").jwt;
+const jwt = require("jsonwebtoken");
+const db = require("../db/db.js");
+const sha1 = require("sha1");
+const rand = require("csprng");
+const formidable = require("formidable");
+const path = require("path");
+const fs = require("fs");
 
 const createToken = (id, name) => {
-  return 'Bearer ' + jwt.sign(
-    {
-      id: id,
-      name: name
-    },
-    secret,
-    { expiresIn: '7d' }
-  )
-}
+  return (
+    "Bearer " +
+    jwt.sign(
+      {
+        id: id,
+        name: name,
+      },
+      secret,
+      { expiresIn: "7d" }
+    )
+  );
+};
 
 function phoneCode(n) {
-  var randomNum = ''
+  var randomNum = "";
   for (var i = 0; i < n; i++) {
-    randomNum += Math.floor(Math.random() * 10)
+    randomNum += Math.floor(Math.random() * 10);
   }
-  return randomNum
+  return randomNum;
 }
 
-const obj = {}
+const obj = {};
 
-router.get('/api/getPhoneCode', (req, res) => {
-  const phone = req.query.phone
-  const code = phoneCode(6)
-  obj[phone] = code
+router.get("/api/getPhoneCode", (req, res) => {
+  const phone = req.query.phone;
+  const code = phoneCode(6);
+  obj[phone] = code;
   res.status(200).send({
-    code
-  })
-})
+    code,
+  });
+});
 
-router.post('/api/login', (req, res) => {
+router.post("/api/login", (req, res) => {
   db.User.findOne({ name: req.body.name }, (err, doc) => {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else if (doc) {
-      if (req.body.type === 'user') {
-        /* // 用户登录
+      if (req.body.type === "user") {
+        /* // user login
         if (obj[req.body.name] !== req.body.code) {
           res.status(200).send({ status: 1, message: 'The verification code of the mobile phone is incorrect' })
           return
         } */
-        const salt = doc.salt
+        const salt = doc.salt;
         if (doc.password === sha1(req.body.password + salt)) {
-          const token = createToken(doc._id, doc.name)
+          const token = createToken(doc._id, doc.name);
           res.status(200).send({
             id: doc._id,
             name: doc.name,
@@ -60,27 +63,27 @@ router.post('/api/login', (req, res) => {
             avatar: doc.avatar,
             userType: doc.userType,
             collectArticle: doc.collectArticle,
-            collectComment: doc.collectComment
-          })
+            collectComment: doc.collectComment,
+          });
         } else {
           res.status(200).send({
             status: 1,
-            message: 'Incorrect password'
-          })
+            message: "Incorrect password",
+          });
         }
       } else {
-        // 管理员登录
-        const salt = doc.salt
+        // admin login
+        const salt = doc.salt;
         if (doc.userType !== 1) {
           res.status(200).send({
             status: 1,
-            message: 'The user is not an administrator'
-          })
+            message: "The user is not an administrator",
+          });
         } else if (doc.password === sha1(req.body.password + salt)) {
-          const token = createToken(doc._id, doc.name)
+          const token = createToken(doc._id, doc.name);
           res.status(200).send({
             status: 0,
-            message: 'Login success',
+            message: "Login success",
             token,
             data: {
               account: doc.name,
@@ -89,9 +92,9 @@ router.post('/api/login', (req, res) => {
               username: doc.name,
               avatar: doc.avatar,
               collectArticle: doc.collectArticle,
-              collectComment: doc.collectComment
-            }
-          })
+              collectComment: doc.collectComment,
+            },
+          });
           /* res.status(200).send({
             id: doc._id,
             name: doc.name,
@@ -104,18 +107,18 @@ router.post('/api/login', (req, res) => {
         } else {
           res.status(200).send({
             status: 1,
-            message: 'Incorrect password'
-          })
+            message: "Incorrect password",
+          });
         }
       }
     } else if (!doc) {
-      if (req.body.type === 'user') {
-        // 用户登录
+      if (req.body.type === "user") {
+        // user login
         /* if (obj[req.body.name] !== req.body.code) {
           res.status(200).send({ status: 1, message: 'The verification code of the mobile phone is incorrect' })
           return
         } */
-        const salt = rand(160, 36)
+        const salt = rand(160, 36);
         const user = {
           name: req.body.name,
           password: sha1(req.body.password + salt),
@@ -123,11 +126,11 @@ router.post('/api/login', (req, res) => {
           userType: 0,
           collectArticle: [],
           collectComment: [],
-          avatar: 'https://demo.buildadmin.com/static/images/avatar.png'
-        }
-        new db.User(user).save().then(user => {
-          const doc1 = user._doc
-          const token = createToken(doc1._id, doc1.name)
+          avatar: "https://demo.buildadmin.com/static/images/avatar.png",
+        };
+        new db.User(user).save().then((user) => {
+          const doc1 = user._doc;
+          const token = createToken(doc1._id, doc1.name);
           res.status(200).send({
             id: doc1._id,
             name: doc1.name,
@@ -135,66 +138,72 @@ router.post('/api/login', (req, res) => {
             avatar: doc1.avatar,
             userType: doc1.userType,
             collectArticle: doc1.collectArticle,
-            collectComment: doc1.collectComment
-          })
-        })
+            collectComment: doc1.collectComment,
+          });
+        });
       } else {
         res.status(200).send({
           status: 1,
-          message: 'The administrator account does not exist'
-        })
+          message: "The administrator account does not exist",
+        });
       }
     }
-  })
-})
+  });
+});
 
-router.get('/api/getAdminAvatar', (req, res) => {
+router.get("/api/getAdminAvatar", (req, res) => {
   db.User.findOne({}, (err, doc) => {
     res.status(200).send({
-      url: doc._doc.avatar
-    })
-  })
-})
+      url: doc._doc.avatar,
+    });
+  });
+});
 
-router.post('/api/changePas', (req, res) => {
-  const salt = rand(160, 36)
+router.post("/api/changePas", (req, res) => {
+  const salt = rand(160, 36);
   const params = {
     password: sha1(req.body.password + salt),
-    salt
-  }
+    salt,
+  };
   db.User.updateOne({ name: req.body.name }, params, (err, data) => {
-    res.status(200).end()
-  })
-})
+    res.status(200).end();
+  });
+});
 
-router.post('/api/changeAvatar', (req, res) => {
+router.post("/api/changeAvatar", (req, res) => {
   const params = {
-    avatar: req.body.avatar
-  }
+    avatar: req.body.avatar,
+  };
   db.User.update({ name: req.body.name }, params, (err, data) => {
     res.status(200).send({
-      url: req.body.avatar
-    })
-  })
-})
+      url: req.body.avatar,
+    });
+  });
+});
 
-router.post('/api/upload', (req, res) => {
-  const form = new formidable.IncomingForm()
-  form.uploadDir = path.join(__dirname, '../public') // 上传图片放置的文件夹
-  form.keepExtensions = true
+router.post("/api/upload", (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.uploadDir = path.join(__dirname, "../public"); // 上传图片放置的文件夹
+  form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
-    const index = files.files.filepath.lastIndexOf('\\')
-    const fileName = files.files.filepath.substring(index + 1)
-    const fileType = files.files.originalFilename.substring(files.files.originalFilename.lastIndexOf('.') + 1)
-    const imgUrl = `http://127.0.0.1:3003/${fileName}.${fileType}`
-    fs.rename(form.uploadDir + '\\' + fileName, form.uploadDir + `\\${fileName}.${fileType}`, err => {
-      if (err) {
-        console.log(err)
-      } else {
-        res.status(200).send({ message: imgUrl })
+    const index = files.files.filepath.lastIndexOf("\\");
+    const fileName = files.files.filepath.substring(index + 1);
+    const fileType = files.files.originalFilename.substring(
+      files.files.originalFilename.lastIndexOf(".") + 1
+    );
+    const imgUrl = `http://127.0.0.1:3003/${fileName}.${fileType}`;
+    fs.rename(
+      form.uploadDir + "\\" + fileName,
+      form.uploadDir + `\\${fileName}.${fileType}`,
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(200).send({ message: imgUrl });
+        }
       }
-    })
-  })
-})
+    );
+  });
+});
 
-module.exports = router
+module.exports = router;
