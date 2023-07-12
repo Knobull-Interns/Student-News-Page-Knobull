48b088a47449f5d719e35040301bea7e56661d46
JahongLiu <jahongliu@gmail.com>
2023-07-11 21:13:33 -0400

    add comment to database realated for intern

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,13 @@
+// This is our file for setting up MongoDB database connection with Mongoose and preset
+// the schemas for : user, category, article, and pay.
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const sha1 = require("sha1");
 const rand = require("csprng");
 // const Sequence = require('./sequence')
 
+// Data Schemas for User
 const UserSchema = new Schema(
   {
     name: String,
@@ -16,14 +20,14 @@ const UserSchema = new Schema(
   },
   { versionKey: false }
 );
-
+// Data Schemas for Category
 const CategorySchema = new Schema(
   {
     name: String,
   },
   { versionKey: false }
 );
-
+// Data Schemas for Article
 const ArticleSchema = new Schema(
   {
     title: String,
@@ -38,7 +42,7 @@ const ArticleSchema = new Schema(
   },
   { versionKey: false }
 );
-
+// Data Schemas for Pay
 const PaySchema = new Schema(
   {
     userId: String,
@@ -47,6 +51,7 @@ const PaySchema = new Schema(
   { versionKey: false }
 );
 
+// This Models obeject maps strings to Mongoose models(collections) based on the schemas above 👆
 const Models = {
   User: mongoose.model("User", UserSchema),
   Article: mongoose.model("Article", ArticleSchema),
@@ -82,6 +87,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/news");
 
 const db = mongoose.connection;
 
+// Simple error handling
 db.on("error", console.error.bind(console, "Database connection error."));
 db.once("open", () => {
   console.log("The database has connected.");
--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,3 +1,8 @@
+// This is our module for connecting MongoDB database using mongoose.
+// We exported this as a function so that when called, it connects MongoDB database.
+// Mongoose is used as the driver, more than that, we used it to pre define schema, as well as use the model
+
+// For Future Interns: don't worry about the mongoose syntax, I just look it up at: mongoosejs.com LOL -Jahong
 module.exports = () => {
   const mongoose = require("mongoose");
   mongoose.connect("mongodb://127.0.0.1:27017/news", {
@@ -8,4 +13,5 @@ module.exports = () => {
     useFindAndModify: true
   });
   // require("require-all")(__dirname + "/../models");
+  // This commented out code can be used for multiple model files. 
 };
